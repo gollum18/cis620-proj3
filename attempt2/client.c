64c47a8cb6bc7518ba646a9796d2749a4ad9f0b9
Christen Ford <christen0ford@gmail.com>
2020-03-28 18:28:14 -0400

    2nd attempt - adds packet wrapping on messages

---
+++ attempt2/client.c
@@ -0,0 +1,382 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/ip.h>
+#include <unistd.h>
+#include <sys/fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+// client defines
+#define BUFMAX 1024
+#define CLIENT_PORT 7776
+#define MAPPER_PORT 21896
+
+// various broadcast addresses
+#define DOT0_BC_ADDR "192.168.0.255" // home
+#define DOT1_BC_ADDR "192.168.1.255" // home
+#define LLAB_BC_ADDR "137.148.254.255" // use when submitting
+
+// the broadcast address (set to one of above)
+#define BROADCAST_ADDR DOT1_BC_ADDR
+
+// packet code defines
+#define PTYPE_REGISTER 0
+#define PTYPE_LOOKUP 10
+#define PTYPE_QUERY 20
+#define PTYPE_UPDATE 30
+#define PTYPE_RECORD 40
+#define PTYPE_ERROR 50
+
+// database command codes
+#define DB_QUERY_CODE 1000
+#define DB_UPDATE_CODE 1001
+
+//
+// packet stuff
+//
+
+// database query type
+struct query_t {
+	int code;
+	int acctnum;
+};
+
+// database update type
+struct update_t {
+	int code;
+	int acctnum;
+	float value;
+};
+
+// database record type
+struct record_t {
+	int acctnum;
+	char name[20];
+	float value;
+	int age;
+};
+
+// allows for sending/receiving fixed sized chunks to/from clients
+// all of these refer to the same region in memory
+union body_t {
+	// used for commands, messages, random data, etc...
+	//	behavior is undefined if this is not NULL terminated
+	char message[BUFMAX/4]; // MUST BE NULL TERMINATED
+	struct query_t query;
+	struct update_t update;
+	struct record_t record;
+};
+
+// send this type to/from clients - best to define in header file
+struct pkt_t {
+	unsigned short ptype;
+	union body_t body;
+};
+
+//
+// PROTOTYPES
+//
+
+void decode_addrstr(char * addrstr, char * ip, unsigned short * port);
+int main(int, char * []);
+void parse_string(char *, char * [], int, char *);
+void print_help();
+void print_record(struct record_t);
+int request_service(char *, struct sockaddr_in *);
+
+//
+// METHODS
+//
+
+/**
+ * Pretty-prints a record to stdout.
+ * @param record The record to print.
+ */
+void print_record(struct record_t record) {
+	printf("-- BEGIN RECORD --\n");
+	printf("\tAcct. Num: %d\n", record.acctnum);
+	printf("\tName: %s\n", record.name);
+	printf("\tValue: %f\n", record.value);
+	printf("\tAge: %d\n", record.age);
+	printf("-- END RECORD --\n\n");
+}
+
+/**
+ * Prints command line help.
+ */
+void print_help() {
+	printf("-- Help --\n");
+	printf("\tquery <acctnum:int>\n");
+	printf("\tupdate <acctnum:int> <value:decimal>\n");
+	printf("\thelp\n");
+	printf("\tquit\n");
+	printf("\n");
+}
+
+/**
+ * Decodes an address string storing the IP address in \'ip\' and
+ * the port in \'port\'.
+ * @param addrstr A pointer to a buffer containing the address
+ * string received from the server.
+ * @param ip A pointer to stored the IP address in.
+ * @param port A pointer to stored the port number in.
+ */
+void decode_addrstr(char * addrstr, char *ip, unsigned short * port) {
+	char * tokens[6];
+	parse_string(addrstr, tokens, 6, ",");
+
+	// BUG REPORT - snprintf does not work here
+	//	will print 127.0.1 into ip not 127.0.1.1
+	for (int i = 0; i < 4; i++) {
+		strcat(ip, tokens[i]);
+		if (i < 3) {
+			strcat(ip, ".");
+		}
+	}
+	*port = (atoi(tokens[4]) * 256) + atoi(tokens[5]);
+}
+
+/**
+ *
+ * @param src The buffer containing the string to parse.
+ * @param dest The destination buffer for src tokens.
+ * @param n The number of tokens to store in dest.
+ * @param delim The tokenization delimeter.
+ */
+void parse_string(char * src, char * dest[], int n, char * delim) {
+	int tokens = 0;
+	char * token = strtok(src, delim);
+	do {
+		dest[tokens++] = token;
+	} while (tokens < n && (token = strtok(NULL, delim)) != NULL);
+}
+
+/**
+ * Requests a service from the service mapper.
+ * @param service The service to request.
+ * @param dest The destination socket address to intialize.
+ * @returns 0 on success, -1 in error.
+ */
+int request_service(char * service, struct sockaddr_in * dest) {
+	struct sockaddr_in local, remote;
+	socklen_t len=sizeof(local), rlen=sizeof(remote);
+	int sk;
+	char sendbuf[BUFMAX], recvbuf[BUFMAX];
+
+	if ((sk = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		perror("socket error");
+		return -1;
+	}
+
+	local.sin_family = AF_INET;
+	local.sin_port = htons(CLIENT_PORT);
+	local.sin_addr.s_addr = INADDR_ANY;
+
+	if (bind(sk, (struct sockaddr *)&local, len) < 0) {
+		perror("bind error");
+		close(sk);
+		return -1;
+	}
+
+	remote.sin_family = AF_INET;
+	remote.sin_port = htons(MAPPER_PORT);
+	remote.sin_addr.s_addr = inet_addr(BROADCAST_ADDR);
+
+	// enable broadcasting on the socket
+	int broadcast = 1;
+	if (setsockopt(sk, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof(broadcast)) < 0) {
+		perror("setsockopt error");
+		close(sk);
+		return -1;
+	}
+
+	// construct a packet to send
+	struct pkt_t pkt;
+
+	pkt.ptype = htons(PTYPE_LOOKUP);
+	memset(pkt.body.message, 0, sizeof(pkt.body.message));
+	snprintf(pkt.body.message, sizeof(pkt.body.message), "GET %s%c", service, '\0');
+	memset(sendbuf, 0, sizeof(sendbuf));
+	memcpy(sendbuf, &pkt, sizeof(struct pkt_t));
+
+	// attempt to send a packet
+	ssize_t net_bytes = 0;
+	if ((net_bytes = sendto(sk, sendbuf, sizeof(struct pkt_t), 0, (struct sockaddr *)&remote, rlen)) < 0) {
+		perror("sendto error");
+		close(sk);
+		return -1;
+	}
+
+	if (net_bytes != sizeof(struct pkt_t)) {
+		perror("sendto error");
+		close(sk);
+		return -1;
+	}
+
+	// attempt to receive a packet
+	if ((net_bytes = recvfrom(sk, recvbuf, sizeof(struct pkt_t), 0, (struct sockaddr *)&remote, &rlen)) < 0) {
+		perror("recvfrom error");
+		close(sk);
+		return -1;
+	}
+
+	if (net_bytes != sizeof(struct pkt_t)) {
+		perror("recvfrom error");
+		close(sk);
+		return -1;
+	}
+
+	// receive over the same packet
+	memcpy(&pkt, recvbuf, sizeof(struct pkt_t));
+	pkt.ptype = ntohs(pkt.ptype);
+
+	// check packet format
+	if (pkt.ptype != PTYPE_LOOKUP) {
+		perror("packet error");
+		close(sk);
+		return -1;
+	}
+
+	close(sk);
+
+	// decode the pkt contents
+	char raddr[24];
+	unsigned short rport;
+	memset(raddr, 0, sizeof(raddr));
+	decode_addrstr(pkt.body.message, raddr, &rport);
+	printf("Service provided by %s at port %d\n", raddr, rport);
+
+	// set the fields in the dest socket address
+	dest->sin_family = AF_INET;
+	dest->sin_port = rport; // port's already in big endian
+	dest->sin_addr.s_addr = inet_addr(raddr);
+
+	return 0;
+}
+
+/**
+ * Entry point of the client program.
+ * @param argc Number of arguments passed via command line.
+ * @param argv Arguments passed via command line.
+ */
+int main(int argc, char * argv[]) {
+	struct sockaddr_in remote;
+	socklen_t rlen=sizeof(remote);
+	char sendbuf[BUFMAX], recvbuf[BUFMAX];
+
+	// attempt to initialize the remote socket
+	if (request_service("CISBANK", &remote) < 0) {
+		perror("request_service error");
+		return 1;
+	}
+
+	int sendmsg = 0;
+	char inbuf[BUFMAX];
+	struct pkt_t pkt;
+	while (1) {
+		printf(">: ");
+		fgets(inbuf, BUFMAX, stdin);
+		if (strlen(inbuf) == 0) {
+			continue;
+		}
+		inbuf[strlen(inbuf)-1] = '\0';
+
+		char * tokens[3];
+		parse_string(inbuf, tokens, 3, " ");
+
+		if (strcmp(tokens[0], "query") == 0) {
+			// construct a query packet, send to server
+			pkt.ptype = htons(PTYPE_QUERY);
+			pkt.body.query.code = htonl(DB_QUERY_CODE);
+			pkt.body.query.acctnum = htonl(atoi(tokens[1]));
+			sendmsg = 1;
+		} else if (strcmp(tokens[0], "update") == 0) {
+			// construct an update packet, send to server
+			pkt.ptype = htons(PTYPE_UPDATE);
+			pkt.body.update.code = htonl(DB_UPDATE_CODE);
+			pkt.body.update.acctnum = htonl(atoi(tokens[1]));
+			float value = strtof(tokens[2], NULL);
+			int * ip = (int *)&value;
+			*ip = htonl(*ip);
+			pkt.body.update.value = value;
+			sendmsg = 1;
+		} else if (strcmp(tokens[0], "help") == 0) {
+			print_help();
+		} else if (strcmp(tokens[0], "quit") == 0) {
+			break;
+		} else {
+			printf("Invalid command entered! Try <help> to see a list of valid commands.\n");
+			continue;
+		}
+
+		if (sendmsg) {
+			struct sockaddr_in local;
+			socklen_t len=sizeof(local);
+			int sk;
+
+			if ((sk = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+				perror("socket error");
+				continue;
+			}
+
+			local.sin_family = AF_INET;
+			local.sin_port = htons(CLIENT_PORT);
+			local.sin_addr.s_addr = INADDR_ANY;
+
+			if (bind(sk, (struct sockaddr *)&local, len) < 0) {
+				perror("bind error");
+				close(sk);
+				continue;
+			}
+
+			if (connect(sk, (struct sockaddr *)&remote, rlen) < 0) {
+				perror("connect error");
+				continue;
+			}
+
+			memset(sendbuf, 0, sizeof(sendbuf));
+			memcpy(sendbuf, &pkt, sizeof(struct pkt_t));
+
+			ssize_t net_bytes = 0;
+			if ((net_bytes = send(sk, sendbuf, sizeof(struct pkt_t), 0)) != sizeof(struct pkt_t)) {
+				perror("send error");
+				continue;
+			}
+
+			if ((net_bytes = recv(sk, recvbuf, sizeof(struct pkt_t), 0)) != sizeof(struct pkt_t)) {
+				perror("recv error");
+				continue;
+			}
+
+			memcpy(&pkt, recvbuf, sizeof(struct pkt_t));
+
+			pkt.ptype = ntohs(pkt.ptype);
+			if (pkt.ptype == PTYPE_RECORD) {
+				// convert the fields to local byte order
+				pkt.body.record.acctnum = ntohl(pkt.body.record.acctnum);
+				pkt.body.record.age = ntohl(pkt.body.record.age);
+				int * ip = (int *)&pkt.body.record.value;
+				*ip = ntohl(*ip);
+
+				print_record(pkt.body.record);
+			} else if (pkt.ptype == PTYPE_UPDATE) {
+				if (strcmp(pkt.body.message, "OK") == 0) {
+					// TODO: Notify the update succeeded
+				} else {
+					printf("Packet Error: %s\n\n", pkt.body.message);
+				}
+			} else if (pkt.ptype == PTYPE_ERROR) {
+				printf("Packet Error: %s\n\n", pkt.body.message);
+			} else {
+				printf("An unexpected error occurred!\n\n");
+			}
+
+			close(sk);
+			sendmsg = 0;
+		}
+	}
+
+	return 0;
+}
---
+++ attempt2/servicemap.c
@@ -0,0 +1,295 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/ip.h>
+#include <unistd.h>
+#include <sys/fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+// service map defines
+#define BUFMAX 1024
+#define NENTRIES 32
+#define NOT_FOUND NENTRIES + 1
+#define PORT 21896
+
+// packet code defines
+#define PTYPE_REGISTER 0
+#define PTYPE_LOOKUP 10
+#define PTYPE_QUERY 20
+#define PTYPE_UPDATE 30
+#define PTYPE_RECORD 40
+#define PTYPE_ERROR 50
+
+//
+// packet stuff
+//
+
+// database query type
+struct query_t {
+	int code;
+	int acctnum;
+};
+
+// database update type
+struct update_t {
+	int code;
+	int acctnum;
+	float value;
+};
+
+// database record type
+struct record_t {
+	int acctnum;
+	char name[20];
+	float value;
+	int age;
+};
+
+// allows for sending/receiving fixed sized chunks to/from clients
+// all of these refer to the same region in memory
+union body_t {
+	char message[BUFMAX/4]; // can be commands, errors, etc...
+	struct query_t query;
+	struct update_t update;
+	struct record_t record;
+};
+
+// send this type to/from clients - best to define in header file
+struct pkt_t {
+	unsigned short ptype; // what data type is stored in the packet
+	union body_t body; // the data stored in the packet
+};
+
+//
+// service cache stuff
+//
+
+struct entry_t {
+	char service[20];
+	char addrstr[24];
+	unsigned short occupied;
+	unsigned long age;
+};
+
+// locally caches services and their addresses for use by 
+//	clients of those services
+static struct entry_t scache[NENTRIES];
+
+//
+// PROTOTYPES
+//
+
+void age_cache();
+char * get_cache(char *);
+unsigned int page_cache();
+void parse_string(char *, char * [], int, char *);
+void put_cache(char *, char *);
+
+//
+// METHODS
+//
+
+/**
+ * Ages the entries in the service cache by one unit.
+ */
+void age_cache() {
+	for (unsigned int i = 0; i < NENTRIES; i++) {
+		if (scache[i].occupied) {
+			scache[i].age++;
+		}
+	}
+}
+
+/**
+ * Attempts to retrieve an entry from the service cache.
+ * @param service A pointer to a buffer containing the service
+ * to lookup.
+ * @return The service address on success, NULL on error.
+ */
+char * get_cache(char * service) {
+	unsigned int pos = NOT_FOUND;
+	
+	for (unsigned int i = 0; i < NENTRIES; i++) {
+		if (scache[i].occupied) {
+			if (strcmp(scache[i].service, service) == 0) {
+				pos = i;
+				break;
+			}
+		}
+	}
+
+	if (pos == NOT_FOUND) {
+		return NULL;
+	} else {
+		scache[pos].age = 0;
+		return scache[pos].addrstr;	
+	}
+}
+
+/**
+ * Pages an entry from the service cache.
+ */
+unsigned int page_cache() {
+	unsigned int pos = 0, age = 0;
+
+	for (unsigned int i = 0; i < NENTRIES; i++) {
+		if (scache[i].age > age) {
+			pos = i;
+			age = scache[i].age;
+		}
+	}
+
+	scache[pos].occupied = 0;
+	scache[pos].age = 0;
+
+	return pos;
+}
+
+/**
+ * Parses a string and stores it in the destination buffer. This
+ * function internally mutates the src string. If you need it 
+ * after calling this function, pass a copy of it to this function.
+ * @param src A pointer to a buffer containing the src string.
+ * @param dest A pointer to a buffer array to write tokens back to.
+ * @param n The number of tokens to store in dest.
+ * @param delim The tokenization delimiter.
+ */
+void parse_string(char * src, char * dest[], int n, char * delim) {
+	int tokens = 0;
+	char * token = strtok(src, delim);
+	do {
+		dest[tokens++] = token;
+	} while (tokens < n && (token = strtok(NULL, delim)) != NULL);
+}
+
+/**
+ * Stores a service/addrstr pair in the service cache.
+ * @param service The LAN unique name of the service.
+ * @param addrstr The LAN unique address string of the server 
+ * providing the service.
+ */
+void put_cache(char * service, char * addrstr) {
+	unsigned int pos = NOT_FOUND;
+
+	age_cache();
+
+	for (unsigned int i = 0; i < NENTRIES; i++) {
+		if (!scache[i].occupied) {
+			pos = i;
+			break;
+		}
+	}
+
+	if (pos == NOT_FOUND) {
+		pos = page_cache();
+	}
+
+	memset(scache[pos].service, 0, sizeof(scache[pos].service));
+	memset(scache[pos].addrstr, 0, sizeof(scache[pos].addrstr));
+	strncpy(scache[pos].service, service, sizeof(scache[pos].service));
+	strncpy(scache[pos].addrstr, addrstr, sizeof(scache[pos].addrstr));
+	scache[pos].occupied = 1;
+	scache[pos].age = 0;
+}
+
+/**
+ * Entry point of the service map.
+ * @param argc Number of args passed via the command line.
+ * @param argv Args passed from the command line.
+ */
+int main(int argc, char * argv[]) {
+	struct sockaddr_in local, remote;
+	socklen_t len=sizeof(local), rlen=sizeof(remote);
+	int sk;
+	char sendbuf[BUFMAX], recvbuf[BUFMAX];
+
+	if ((sk = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		perror("socket error");
+		return 1;
+	}
+
+	local.sin_family = AF_INET;
+	local.sin_port = htons(PORT);
+	local.sin_addr.s_addr = INADDR_ANY;
+
+	if (bind(sk, (struct sockaddr *)&local, len) < 0) {
+		perror("bind error");
+		close(sk);
+		return 1;
+	}
+
+	while (1) {
+		// these get written to/read from sendbuf/recvbuf
+		struct pkt_t pkt;
+		
+		memset(pkt.body.message, 0, sizeof(pkt.body.message));
+		memset(sendbuf, 0, sizeof(sendbuf));
+		memset(recvbuf, 0, sizeof(recvbuf));
+
+		ssize_t net_bytes = 0;
+		if ((net_bytes = recvfrom(sk, recvbuf, sizeof(struct pkt_t), 0, (struct sockaddr *)&remote, &rlen)) < 0) {
+			perror("recvfrom error");
+			continue;
+		}
+
+		if (net_bytes != sizeof(struct pkt_t)) {
+			perror("recvfrom error");
+			continue;
+		}
+
+		memcpy(&pkt, recvbuf, sizeof(struct pkt_t));
+		pkt.ptype = ntohs(pkt.ptype);
+		printf("Received from %s: %s\n", inet_ntoa(remote.sin_addr), pkt.body.message);
+
+		if (pkt.ptype == PTYPE_REGISTER) {
+			char * tokens[3];
+			parse_string(pkt.body.message, tokens, 3, " ");
+
+			if (strcmp(tokens[0], "PUT") == 0) {
+				put_cache(tokens[1], tokens[2]);
+
+				pkt.ptype = htons(PTYPE_REGISTER);
+				memset(pkt.body.message, 0, sizeof(pkt.body.message));
+				strcpy(pkt.body.message, "OK");
+			} else {
+				pkt.ptype = PTYPE_ERROR;
+			}
+		} else if (pkt.ptype == PTYPE_LOOKUP) {
+			char * tokens[2];
+			parse_string(pkt.body.message, tokens, 2, " ");
+
+			if (strcmp(tokens[0], "GET") == 0) {
+				char * addrstr;
+				if ((addrstr = get_cache(tokens[1])) != NULL) {
+					pkt.ptype = htons(PTYPE_LOOKUP);
+					memset(pkt.body.message, 0, sizeof(pkt.body.message));
+					strcpy(pkt.body.message, addrstr);
+				} else {
+					pkt.ptype = PTYPE_ERROR;
+				}
+			} else {
+				pkt.ptype = PTYPE_ERROR;
+			}
+		} else {
+			pkt.ptype = PTYPE_ERROR;
+		}
+
+		// check if there was an error - overwrite code
+		if (pkt.ptype == PTYPE_ERROR) {
+			pkt.ptype = htons(pkt.ptype);
+			strcpy(pkt.body.message, "FAIL");
+		}
+
+		// write back the response packet		
+		memcpy(sendbuf, &pkt, sizeof(struct pkt_t));
+
+		if (sendto(sk, sendbuf, sizeof(struct pkt_t), 0, (struct sockaddr *)&remote, rlen) < 0) {
+			perror("sendto error");
+			continue;
+		}
+	}
+
+	return 0;
+}
